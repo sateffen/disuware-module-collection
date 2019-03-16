82389dfa83d349f172804f99c5b22f7eed4de67d
sateffen <sateffen@gmail.com>
2019-03-16 19:14:49 +0100

    Removed graphqlprovider

--- interfaces/graphqlprovider.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "disuware!graphqlprovider" {
-    type GraphQLSchema = Object;
-
-    export function executeGraphQLQuery(query: string, variables?: Object): Promise<Object>;
-    export function addGraphQLSchema(schema: GraphQLSchema): void;
-    export function addRawGraphQLSchema(typeDefs: string, resolvers: Object): void;
-}
